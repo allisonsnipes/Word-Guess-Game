8d87aac1d2e043a801706142b403e9a97abb142e
Allison Snipes <ajsnipes@usfca.edu>
2019-03-18 20:42:13 -0700

    layout done start hangman part

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,25 @@
 
 $(document).ready(function({
 
+  //starts the game
+  startGame(;)
+  //resets the game
+  resetGame();
+  //exit game on quit
+  quitGame();
 }));
 
-//starting bit
+//starting defining starting place
+let currentQuestion = 0,
+  score = 0,
+  guessingWords = [
+
+  ];
+
 
 //butons on the bottom
-resetButton.parentNode.removeChild(resetButton)
+
+//resets the game
+$(".resetGame").on("click", functino(event) {
+  $(".writeup").hide();
+});
