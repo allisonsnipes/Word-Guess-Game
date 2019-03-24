358b9bfcc34a015c568c77ca34d2d50c23243a28
Allison Snipes <ajsnipes@usfca.edu>
2019-03-23 22:30:37 -0700

    finished but not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,19 +11,21 @@ $(document).ready(function() {
 });
 
 //starting defining variables and starting points
-let currentQuestion = 0,
-  score = 0,
-  alphaB = [
+let currentQuestion = 0, //current question number
+  score = 0, //begin score @ 0
+  alphaB = [ //creates alphabet
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-    guesses = [ ],
-    guess = 10,
-    words = ["vault7", "davinchi", "trojan", "fatVirus", "polymorphicVirus", "macroVirus","fileInfector", "browserHijacker", "directActionVirus","bootSectorVirus"],
-    wins = 0,
-    losses = 0,
-    lives = 10,
-    counter = 0,
-    spacesInWord;
+    guesses = [ ], //stored guesses of users
+    guess = 10, //stores guess count limit 10
+    words, //current word
+    wins = 0, //start with 0 wins counter
+    losses = 0, //start 0 losses counter
+    lives = 10, //start with specific #of lives
+    counter = 0, //counter increment
+    spacesInWord; //spaces in actual word
+
+//beginning base of code--- set all elements up with a purpose...
 
 //creating the alphaB buttons ul list
 let buttons = function() {
@@ -40,16 +42,67 @@ let buttons = function() {
     letters.appendChild(list);
   }
 
-//creating the word elements
+//DOM display for words to guess connected to the ul element
+result = function() {
+  wordHolder = document.getElementById("hold");
+    correctChoice = document.createElement("ul");
 
-// creating the number lives
-let
+    for (let i = 0; i < words.length; i++) {
+      correctChoice.setAttribute("id", "my-word");
+      guess = document.createElement("li");
+      guess.setAttribute("class", "guess");
+      if(word[i] === "-") {
+        guess.innerHTML = "-";
+        spacesInWord = 1;
+      } else {
+        guess.innerHTML = "_";
+      }
 
-//display lives
-let showingLives = document.getElementsById("lives");
-
+      guesses.push(guess)//makes guesses populate
+      wordHolder.appendChild(correct);
+      correct.appendChild(guess);
+    }
 }
 
+//creating the number lives and display that number
+let showingLives = document.getElementsById("lives");
+
+//displaying messages depending on # of lives
+comments = function() {
+  showingLives.innerHTML = lives + "left";
+  if (lives < 1) {
+    showingLives.innerHTML = "Lulz, you lost. Game over";
+  }
+  for (let i = 0; i < guesses.length; i++) {
+    if (counter + space === guesses.length) {
+      showingLives.innerHTML = "Congrats you win";
+    }
+  }
+}
+
+//onclick function for lives decrementing
+check = function() {
+  list.onclick = function() {
+    let guess = (this.innerHTML);
+    this.setAttribute("class", "active");
+    this.onclick = null;
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === guess) {
+        guesses[i].innerHTML = guess;
+        counter += 1;
+      }
+    }
+    var k = (word.indexOf(guess));
+    if (k === -1) {
+      lives -= 1;
+      comments();
+    } else {
+      comments();
+    }
+  }
+}
+
+//important button functions
 //function to start game
 function start() {
   $(".startGame").on("click", function(event) {
@@ -72,11 +125,4 @@ function quit() {
   });
 }
 
-//DOM display of various words to guess
-function wordsToGuess() {
-  $(".wordsHere").html(`
-    <legend>
-      ${words[currentWord].word}
-    </legend>
-  `);
-}
+// ["vault7", "davinchi", "trojan", "fatVirus", "polymorphicVirus", "macroVirus","fileInfector", "browserHijacker", "directActionVirus","bootSectorVirus"]
